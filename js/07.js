f13c46c99c80374e6afc4f0f6967ad8beda46236
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 02:25:48 -0300

    109 - Controlar el orden de las operaciones

---
+++ js/07.js
@@ -0,0 +1,23 @@
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (600+600) * 1.05;
+
+
+// console.log(resultado)
+
+let puntaje = 10
+
+// Incremento o resto el valor en 1
+
+puntaje++ // Le sumo 1 a 10 = 11
+puntaje-- // Le resto 1 a 11 = 10
+
+//Incremento o resto el valor con un número en específico
+
+puntaje -= 10 // Le resto 10 a 10
+puntaje += 5 // Le sumo 5 a 0
+
+
+console.log(puntaje)
