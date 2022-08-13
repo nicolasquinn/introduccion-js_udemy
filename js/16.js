2fb7056293655169dfe6ba0e8a6245a06cd51a1b
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 18:31:03 -0300

    120 - Funciones en JavaScript

---
+++ js/16.js
@@ -0,0 +1,15 @@
+// Declaración de una función
+
+function funcionDePrueba() {
+    console.log(10 + 10);
+}
+
+funcionDePrueba();
+
+// Expresión de la función
+
+const funcio2nDePrueba = function() {
+    console.log(3 + 3);
+}
+
+funcionDePrueba2();
