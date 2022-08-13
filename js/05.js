b87654f0edcf40c770f568e259ab069e07f9974e
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 00:33:04 -0300

    107 - Números y operadores

---
+++ js/05.js
@@ -0,0 +1,17 @@
+// Numeros
+
+// JavaScript no tiene numeros enteros o flotantes, no existen los valores de "float". Básicamente siempre se declaran igual y son tipo number.
+
+const numeroUno = 100;
+const numeroDos = 200;
+const numeroTres = 5;
+const numeroCuatro = -10;
+const numeroSeis = 3;
+const numeroCinco = 10;
+
+console.log(numeroUno + numeroDos); // Puedo sumar 2 variables, obviamente estas tienen que ser ambas con un valor tipo number.
+console.log(numeroUno - numeroDos); // Puedo restarlas
+console.log(numeroUno * numeroDos); // Puedo multiplicarlas
+console.log(numeroCinco / numeroTres); // Puedo dividirlas
+console.log(numeroCinco % numeroTres) // Puedo obtener el residuo/módulo/dividendo, en este caso sería 0.
+console.log(numeroCinco % numeroSeis) // En este caso obtendría 1, porque 10/3 = 3 y me sobra 1.
