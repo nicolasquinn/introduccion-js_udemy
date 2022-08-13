52eee2f945f531be4c59183bb18c43aa1806b13a
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 03:07:37 -0300

    113 - Modificar objetos

--- js/10.js
+++ js/10.js
@@ -6,7 +6,16 @@ const producto = {
     disponible : true // Acá no es necesaria la coma, ya que es la última
 }
 
-console.log(producto); // Print de la lista entera
-console.log(producto.precio); // Uso el .variable para printear el valor de la misma
-console.log(producto["precio"]); // Cumple la misma función que antes sólo que con otra sintaxis, no se suele usar mucho.
+// console.log(producto); // Print de la lista entera
+// console.log(producto.precio); // Uso el .variable para printear el valor de la misma
+// console.log(producto["precio"]); // Cumple la misma función que antes sólo que con otra sintaxis, no se suele usar mucho.
 
+// Agregar variables con valores a un objeto
+
+producto.imagen = 'imagen.jpg'; // Agrego la variante después del producto. , en este caso sería imagen el nombre de la misma. Seguidamente coloco el valor entre '' o "".
+
+// Remover variables
+
+delete producto.disponible; // Declaro delete para borrar, seguidamente coloco "nombredelobjeto.nombredelavariante" para borrar la variante deseada en determinado objeto.
+
+console.log(producto);
