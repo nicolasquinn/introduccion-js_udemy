91df936664094326bd587254b849bee82a58aea6
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 16:40:05 -0300

    117 - Arrays

---
+++ js/14.js
@@ -0,0 +1,22 @@
+// Arrays o arreglos
+
+// const numeros = [10, 20, 30, 40, 50]; // El array se especifica entre [], dentro de este irán los elementos separados entre ,
+// const random = [10, "Nicolas", true, null, 550, "Manteca"] // Dentro de un array puede haber cualquier tipo de valor, ya sea un str, number, boolean, etc.
+
+// console.log(random); // Hago un print normal del array random
+// console.table(random); // Hago un print en forma de table del array random, en el cual me va mostrar todos los valores con un índice
+// console.log(random.length) // Hago un print de la cantidad de elementos / extensión que tengo en un array, en este caso 6.
+// console.log(numeros[3]); // Hago un print específico del índice declarado, en este caso me haría un print de 40 ya que es el índice 3 (como en la mayoría de lenguajes, el índice empieza a contar desde 0)
+// console.log(numeros[1]);
+// console.log(numeros[1]); 
+// console.log(numeros[2]);
+// console.log(numeros[4]);
+
+// En vez de hacer prints 1 por 1 de un índice se puede hacer lo siguiente 
+// Esto es una iterador
+
+// numeros.forEach( function(numeros) {
+//     console.log(numeros)
+// })
+
+
