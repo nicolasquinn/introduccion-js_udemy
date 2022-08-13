18a1c099712aeaf8822c487c68bf4c1661c6f359
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-12 23:55:19 -0300

    104 - Variables con let y const

---
+++ js/02.js
@@ -0,0 +1,17 @@
+let producto = "Manteca"; // Declario lletiable conlet", seguidamente el nombre de la misma "producto" y el valor va después del =, en este caso el valor sería "manteca" y es de tipo STRING.
+
+console.log(producto) // Hago un print con "console.log()", dentro de los paréntesis puedo colocarle el nombre de una variable para que haga dicha acción.
+
+let disponible; // Puedo crear una variable que no tenga ningún tipo de valor, simplemente para darle un uso después.
+
+let productoUno = "Leche",
+    productoDos = "Harina",
+    productoTres = "Huevos"; // Puedo declarar más de 1 variable simplemente separando estas
+
+// Estilos de escritura para las variables
+let nombre_producto = 'Producto nombre' // Underscore
+let nombreProducto = 'Producto nombre' // Camel case
+let NombreProducto = 'Producto nombre' // Pascal case
+let nombreproducto = 'Producto nombre' // Ninguna
+
+
---
+++ js/03.js
@@ -0,0 +1,21 @@
+const producto = "Manteca"; 
+
+console.log(producto) 
+
+// const disponible; // No puedo declarar una variable const sin un valor
+
+const disponible = true
+
+// const disponible = false // Tampoco le puedo modificar el valor
+
+const productoUno = "Leche",
+    productoDos = "Harina",
+    productoTres = "Huevos"; 
+
+
+const nombre_producto = 'Producto nombre' 
+const nombreProducto = 'Producto nombre'
+const NombreProducto = 'Producto nombre' 
+const nombreproducto = 'Producto nombre' 
+
+
