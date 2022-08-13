215a33bf05ec7e8ae16e3bde1d9785c5f0081625
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 18:40:47 -0300

    121 - Diferencias entre el tipo de funciones

--- js/16.js
+++ js/16.js
@@ -1,15 +1,21 @@
 // Declaración de una función
 
+funcionDePrueba(); // Esta si aplica porque funcionDePrueba es una variable como tal y se puede ejecutar previo a haberla declarado.
+
 function funcionDePrueba() {
     console.log(10 + 10);
 }
 
-funcionDePrueba();
-
 // Expresión de la función
 
-const funcio2nDePrueba = function() {
-    console.log(3 + 3);
-}
+// funcionDePrueba2(); // Esto va dar un error, ya que estoy ejecutando la función previo a haber declarado una variable q
 
-funcionDePrueba2();
+funcionDePrueba2() // Ejecutar la función que está declarada dentro de una variable previo a la misma me va dar un error.
+
+const funcionDePrueba2 = function() {
+    console.log(3 + 3);
+} // Esta no es una función como tal, ya que está declarada desde un inicio como una variable const.
+
+// Pongo esto como ejemplo porque es exactamente lo mismo, osea, estoy ejecutando una variable sin haberla declarado primero, tendría que ser al revés. Es decir, declaro la variable y luego la ejecuto.
+console.log(prueba)
+const prueba = 3 + 3 
