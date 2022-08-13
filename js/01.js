8d0e6c2e4a058fd52034efe5010cf6f539f96716
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-12 23:41:33 -0300

    103 - Variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,17 @@
+var producto = "Manteca"; // Declario la variable con "var", seguidamente el nombre de la misma "producto" y el valor va después del =, en este caso el valor sería "manteca" y es de tipo STRING.
+
+console.log(producto) // Hago un print con "console.log()", dentro de los paréntesis puedo colocarle el nombre de una variable para que haga dicha acción.
+
+var disponible; // Puedo crear una variable que no tenga ningún tipo de valor, simplemente para darle un uso después.
+
+var productoUno = "Leche",
+    productoDos = "Harina",
+    productoTres = "Huevos"; // Puedo declarar más de 1 variable simplemente separando estas
+
+// Estilos de escritura para las variables
+var nombre_producto = 'Producto nombre' // Underscore
+var nombreProducto = 'Producto nombre' // Camel case
+var NombreProducto = 'Producto nombre' // Pascal case
+var nombreproducto = 'Producto nombre' // Ninguna
+
+
