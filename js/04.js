12d1c3a39321f3c96ed834032726a88964086f38
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 00:06:07 -0300

    105 - Strings o cadenas de texto

---
+++ js/04.js
@@ -0,0 +1,5 @@
+// Strings o cadenas de texto
+
+const producto = 'Monitor de 20"' // Al asignarle un valor string entre '' no es necesario marcar las "" con el \
+// const producto = "Monitor de 20\" asdasda" // En el hipotético caso de que tenga que usar las "" para declarar mi string para que se muestren las "" extra tengo que poner un \
+console.log(producto)
