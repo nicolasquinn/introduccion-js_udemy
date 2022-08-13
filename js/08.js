460eb1494a276ad04137ad2cde0556a9b3e9b56e
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-13 02:42:09 -0300

    110 - Concatenación y template strings

---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = "Nicolas";
+const email = "correo@correo.com"
+
+// Concatenación OBSOLETA, no se usa más.
+// console.log("Nombre " + nombre + " " + "Email " + email)
+
+// Template strings - String literals es lo que se usa ahora.
+console.log(`Nombre: ${nombre} Correo: ${email}`)
