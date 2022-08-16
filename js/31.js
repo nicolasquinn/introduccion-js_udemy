183220a3f7b0a217b7404e2920f510b49d537df8
Nicolas Quinn <nclsquinn@gmail.com>
2022-08-15 22:12:06 -0300

    139 - Notification API

---
+++ js/31.js
@@ -0,0 +1,12 @@
+
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', function() {
+    Notification.requestPermission()
+        .then(resultado => console.log(`el resultado es ${resultado}`))
+})
+
+if(Notification.permission == 'granted') {
+    new Notification('Esta es una notificación', {
+        body: 'Nicolás Quinn'
+    })
+}
